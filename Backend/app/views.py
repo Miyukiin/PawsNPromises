78a628042509cb780e47b28a5d431369673d72a9
HolyShaq <acebyronehalili@gmail.com>
2024-12-08 00:24:53 +0800

    feat: add get pets endpoint

--- Backend/app/views.py
+++ Backend/app/views.py
@@ -1,12 +1,28 @@
-from django.shortcuts import render
-from django.http import HttpRequest, JsonResponse
+from django.http import HttpRequest
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from app.models import Pet
 
 import logging
 logger = logging.getLogger(__name__)
 
 # Create your views here.
+
+@api_view(['GET'])
+def get_pets(request:HttpRequest):
+    pets = Pet.objects.all()
+    response = []
+    for index, pet in enumerate(pets):
+        response.append({
+            'id': index,
+            'name': pet.name,
+            'age': pet.age.name,
+            'breed': pet.breed.name,
+            'imageSrc': "/image/default-image.png",
+        })
+    if request.method == "GET":
+        return Response({'pets': response})
+    return Response({'message': 'Not Get Method'})
 
 @api_view(['GET'])
 def sample_api(request:HttpRequest):
